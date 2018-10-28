aca765eb55261f5afa2d479eba1cc3f8bbba41d6
basch1727 <basch19900@gmail.com>
2018-10-28 18:15:55 +0300

    feat: add a few tests

--- make/test.js
+++ make/test.js
@@ -4,12 +4,24 @@ const { make } = require('./src/make.js');
 function sum(a, b) {
   return a + b;
 }
-describe('make', function () {
-  it('task numbers', function () {
+describe('make', () => {
+  it('taskNumbers', () => {
     assert.equal(make(15)(34, 21, 666)(41)(sum), 777);
   });
 
-  it('if empty', function () {
+  it('if empty', () => {
     assert.equal(make(15)(34, 21, 666)()(41)()(sum), 777);
+  });
+
+  it('task numbers', () => {
+    assert.equal(make(241)(604, 555, 3211)(111)(sum), 4722);
+  });
+
+  it('task numbers', () => {
+    assert.equal(make(777777)(777777, 777777, 777777)(777777)(sum), 3888885);
+  });
+
+  it('task numbers', () => {
+    assert.equal(make(null)(34, -21, 666)(41)(sum), 720);
   });
 });
